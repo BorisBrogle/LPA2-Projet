7d3bb4fe637fe27fb09f87711be502486cd424b9
boris.brogle <boris.brogle@gmail.com>
2017-12-02 06:18:13 -0500

    Changing the tests, adding vcr

--- test/controllers/towns_controller_test.rb
+++ test/controllers/towns_controller_test.rb
@@ -1,49 +1,66 @@
 require 'test_helper'
 
 class TownsControllerTest < ActionController::TestCase
-  setup do
-    @town = towns(:one)
-  end
+ setup do
+   @town = towns(:one)
+   @inexistingTown = towns(:inexisting)
+ end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:towns)
-  end
+ test "should get index" do
+   get :index
+   assert_response :success
+   assert_not_nil assigns(:towns)
+ end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+ test "should get new" do
+   get :new
+   assert_response :success
+ end
 
-  test "should create town" do
-    assert_difference('Town.count') do
-      post :create, town: { lat: @town.lat, lon: @town.lon, name: @town.name }
-    end
+ test "should create town" do
+   VCR.use_cassette("town_ok") do
+     assert_difference('Town.count') do
+       post :create, town: { name: @town.name }
+     end
+   end
 
-    assert_redirected_to town_path(assigns(:town))
-  end
+   assert_redirected_to town_path(assigns(:town))
+ end
+ 
+ test "should fail if town does not exist" do
+   VCR.use_cassette("inexistingTown_ok") do
+     assert_no_difference('Town.count') do
+       post :create, town: { name: @inexistingTown.name }
+     end
+   end
 
-  test "should show town" do
-    get :show, id: @town
-    assert_response :success
-  end
+   assert_response :success
+ end
 
-  test "should get edit" do
-    get :edit, id: @town
-    assert_response :success
-  end
+ test "should show town" do
+   VCR.use_cassette("showTown_ok") do
+     get :show, id: @town
+     assert_response :success
+   end
+ end
 
-  test "should update town" do
+ test "should get edit" do
+   get :edit, id: @town
+   assert_response :success
+ end
+
+ test "should update town" do
+   VCR.use_cassette("updateTown_ok") do
     patch :update, id: @town, town: { lat: @town.lat, lon: @town.lon, name: @town.name }
     assert_redirected_to town_path(assigns(:town))
-  end
+   end
+ end
 
-  test "should destroy town" do
-    assert_difference('Town.count', -1) do
-      delete :destroy, id: @town
-    end
+ test "should destroy town" do
+   assert_difference('Town.count', -1) do
+     delete :destroy, id: @town
+   end
 
-    assert_redirected_to towns_path
-  end
+   assert_redirected_to towns_path
+ end
 end
